53be522bb5c561c5edd80f219786736e2998f46b
Hijiri <75280315+hijjiri@users.noreply.github.com>
2021-01-25 17:50:00 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,573 @@
+'use strict'
+function FrameClick(FrameName) {
+  switch (FrameName){
+    case 'A':
+      var elem = document.getElementById("ContentFrameA");
+
+      if (elem.classList.contains("CCFrameAZoom") == true) {
+        elem.classList.remove("CCFrameAZoom");
+      } else {
+        elem.classList.add("CCFrameAZoom");
+      }
+      break;
+    case 'B':
+      var elem = document.getElementById("ContentFrameB");
+      if (elem.classList.contains("CCFrameBZoom") == true) {
+        elem.classList.remove("CCFrameBZoom");
+      } else {
+        elem.classList.add("CCFrameBZoom");
+      }
+      break;
+    case 'C':
+      var elem = document.getElementById("ContentFrameC");
+      if (elem.classList.contains("CCFrameCZoom") == true) {
+        elem.classList.remove("CCFrameCZoom");
+      } else {
+        elem.classList.add("CCFrameCZoom");
+      }
+      break;
+    case 'D':
+      var elem = document.getElementById("ContentFrameD");
+      if (elem.classList.contains("CCFrameDZoom") == true) {
+        elem.classList.remove("CCFrameDZoom");
+      } else {
+        elem.classList.add("CCFrameDZoom");
+      }
+      break;
+    case 'E':
+      var elem = document.getElementById("ContentFrameE");
+      if (elem.classList.contains("CCFrameEZoom") == true) {
+        elem.classList.remove("CCFrameEZoom");
+      } else {
+        elem.classList.add("CCFrameEZoom");
+      }
+      break;
+    case 'F':
+      var elem = document.getElementById("ContentFrameF");
+      if (elem.classList.contains("CCFrameFZoom") == true) {
+        elem.classList.remove("CCFrameFZoom");
+      } else {
+        elem.classList.add("CCFrameFZoom");
+      }
+      break;
+    case 'G':
+      var elem = document.getElementById("ContentFrameG");
+      if (elem.classList.contains("CCFrameGZoom") == true) {
+        elem.classList.remove("CCFrameGZoom");
+      } else {
+        elem.classList.add("CCFrameGZoom");
+      }
+      break;
+    case 'H':
+      var elem = document.getElementById("ContentFrameH");
+      if (elem.classList.contains("CCFrameHZoom") == true) {
+        elem.classList.remove("CCFrameHZoom");
+      } else {
+        elem.classList.add("CCFrameHZoom");
+      }
+      break;
+    case 'I':
+      var elem = document.getElementById("ContentFrameI");
+      if (elem.classList.contains("CCFrameIZoom") == true) {
+        elem.classList.remove("CCFrameIZoom");
+      } else {
+        elem.classList.add("CCFrameIZoom");
+      }
+      break;
+    case 'J':
+      var elem = document.getElementById("ContentFrameJ");
+      if (elem.classList.contains("CCFrameJZoom") == true) {
+        elem.classList.remove("CCFrameJZoom");
+      } else {
+        elem.classList.add("CCFrameJZoom");
+      }
+      break;
+    case 'K':
+      var elem = document.getElementById("ContentFrameK");
+      if (elem.classList.contains("CCFrameKZoom") == true) {
+        elem.classList.remove("CCFrameKZoom");
+      } else {
+        elem.classList.add("CCFrameKZoom");
+      }
+      break;
+    case 'L':
+      var elem = document.getElementById("ContentFrameL");
+      if (elem.classList.contains("CCFrameLZoom") == true) {
+        elem.classList.remove("CCFrameLZoom");
+      } else {
+        elem.classList.add("CCFrameLZoom");
+      }
+      break;
+  }
+}
+function FrameClick2(FrameName) {
+  switch (FrameName){
+    case '2A':
+      var elem = document.getElementById("ContentFrame2A");
+
+      if (elem.classList.contains("CCFrameAZoom") == true) {
+        elem.classList.remove("CCFrameAZoom");
+      } else {
+        elem.classList.add("CCFrameAZoom");
+      }
+      break;
+    case '2B':
+      var elem = document.getElementById("ContentFrame2B");
+      if (elem.classList.contains("CCFrameBZoom") == true) {
+        elem.classList.remove("CCFrameBZoom");
+      } else {
+        elem.classList.add("CCFrameBZoom");
+      }
+      break;
+    case '2C':
+      var elem = document.getElementById("ContentFrame2C");
+      if (elem.classList.contains("CCFrameCZoom") == true) {
+        elem.classList.remove("CCFrameCZoom");
+      } else {
+        elem.classList.add("CCFrameCZoom");
+      }
+      break;
+    case '2D':
+      var elem = document.getElementById("ContentFrame2D");
+      if (elem.classList.contains("CCFrameDZoom") == true) {
+        elem.classList.remove("CCFrameDZoom");
+      } else {
+        elem.classList.add("CCFrameDZoom");
+      }
+      break;
+    case '2E':
+      var elem = document.getElementById("ContentFrame2E");
+      if (elem.classList.contains("CCFrameEZoom") == true) {
+        elem.classList.remove("CCFrameEZoom");
+      } else {
+        elem.classList.add("CCFrameEZoom");
+      }
+      break;
+    case '2F':
+      var elem = document.getElementById("ContentFrame2F");
+      if (elem.classList.contains("CCFrameFZoom") == true) {
+        elem.classList.remove("CCFrameFZoom");
+      } else {
+        elem.classList.add("CCFrameFZoom");
+      }
+      break;
+    case '2G':
+      var elem = document.getElementById("ContentFrame2G");
+      if (elem.classList.contains("CCFrameGZoom") == true) {
+        elem.classList.remove("CCFrameGZoom");
+      } else {
+        elem.classList.add("CCFrameGZoom");
+      }
+      break;
+    case '2H':
+      var elem = document.getElementById("ContentFrame2H");
+      if (elem.classList.contains("CCFrameHZoom") == true) {
+        elem.classList.remove("CCFrameHZoom");
+      } else {
+        elem.classList.add("CCFrameHZoom");
+      }
+      break;
+    case '2I':
+      var elem = document.getElementById("ContentFrame2I");
+      if (elem.classList.contains("CCFrameIZoom") == true) {
+        elem.classList.remove("CCFrameIZoom");
+      } else {
+        elem.classList.add("CCFrameIZoom");
+      }
+      break;
+    case '2J':
+      var elem = document.getElementById("ContentFrame2J");
+      if (elem.classList.contains("CCFrameJZoom") == true) {
+        elem.classList.remove("CCFrameJZoom");
+      } else {
+        elem.classList.add("CCFrameJZoom");
+      }
+      break;
+    case '2K':
+      var elem = document.getElementById("ContentFrame2K");
+      if (elem.classList.contains("CCFrameKZoom") == true) {
+        elem.classList.remove("CCFrameKZoom");
+      } else {
+        elem.classList.add("CCFrameKZoom");
+      }
+      break;
+    case '2L':
+      var elem = document.getElementById("ContentFrame2L");
+      if (elem.classList.contains("CCFrameLZoom") == true) {
+        elem.classList.remove("CCFrameLZoom");
+      } else {
+        elem.classList.add("CCFrameLZoom");
+      }
+      break;
+  }
+}
+function FrameClick3(FrameName) {
+  switch (FrameName){
+    case '3A':
+      var elem = document.getElementById("ContentFrame3A");
+
+      if (elem.classList.contains("CCFrameAZoom") == true) {
+        elem.classList.remove("CCFrameAZoom");
+      } else {
+        elem.classList.add("CCFrameAZoom");
+      }
+      break;
+    case '3B':
+      var elem = document.getElementById("ContentFrame3B");
+      if (elem.classList.contains("CCFrameBZoom") == true) {
+        elem.classList.remove("CCFrameBZoom");
+      } else {
+        elem.classList.add("CCFrameBZoom");
+      }
+      break;
+    case '3C':
+      var elem = document.getElementById("ContentFrame3C");
+      if (elem.classList.contains("CCFrameCZoom") == true) {
+        elem.classList.remove("CCFrameCZoom");
+      } else {
+        elem.classList.add("CCFrameCZoom");
+      }
+      break;
+    case '3D':
+      var elem = document.getElementById("ContentFrame3D");
+      if (elem.classList.contains("CCFrameDZoom") == true) {
+        elem.classList.remove("CCFrameDZoom");
+      } else {
+        elem.classList.add("CCFrameDZoom");
+      }
+      break;
+    case '3E':
+      var elem = document.getElementById("ContentFrame3E");
+      if (elem.classList.contains("CCFrameEZoom") == true) {
+        elem.classList.remove("CCFrameEZoom");
+      } else {
+        elem.classList.add("CCFrameEZoom");
+      }
+      break;
+    case '3F':
+      var elem = document.getElementById("ContentFrame3F");
+      if (elem.classList.contains("CCFrameFZoom") == true) {
+        elem.classList.remove("CCFrameFZoom");
+      } else {
+        elem.classList.add("CCFrameFZoom");
+      }
+      break;
+    case '3G':
+      var elem = document.getElementById("ContentFrame3G");
+      if (elem.classList.contains("CCFrameGZoom") == true) {
+        elem.classList.remove("CCFrameGZoom");
+      } else {
+        elem.classList.add("CCFrameGZoom");
+      }
+      break;
+    case '3H':
+      var elem = document.getElementById("ContentFrame3H");
+      if (elem.classList.contains("CCFrameHZoom") == true) {
+        elem.classList.remove("CCFrameHZoom");
+      } else {
+        elem.classList.add("CCFrameHZoom");
+      }
+      break;
+    case '3I':
+      var elem = document.getElementById("ContentFrame3I");
+      if (elem.classList.contains("CCFrameIZoom") == true) {
+        elem.classList.remove("CCFrameIZoom");
+      } else {
+        elem.classList.add("CCFrameIZoom");
+      }
+      break;
+    case '3J':
+      var elem = document.getElementById("ContentFrame3J");
+      if (elem.classList.contains("CCFrameJZoom") == true) {
+        elem.classList.remove("CCFrameJZoom");
+      } else {
+        elem.classList.add("CCFrameJZoom");
+      }
+      break;
+    case '3K':
+      var elem = document.getElementById("ContentFrame3K");
+      if (elem.classList.contains("CCFrameKZoom") == true) {
+        elem.classList.remove("CCFrameKZoom");
+      } else {
+        elem.classList.add("CCFrameKZoom");
+      }
+      break;
+    case '3L':
+      var elem = document.getElementById("ContentFrame3L");
+      if (elem.classList.contains("CCFrameLZoom") == true) {
+        elem.classList.remove("CCFrameLZoom");
+      } else {
+        elem.classList.add("CCFrameLZoom");
+      }
+      break;
+  }
+}
+function FrameClick4(FrameName) {
+  switch (FrameName){
+    case '4A':
+      var elem = document.getElementById("ContentFrame4A");
+
+      if (elem.classList.contains("CCFrameAZoom") == true) {
+        elem.classList.remove("CCFrameAZoom");
+      } else {
+        elem.classList.add("CCFrameAZoom");
+      }
+      break;
+    case '4B':
+      var elem = document.getElementById("ContentFrame4B");
+      if (elem.classList.contains("CCFrameBZoom") == true) {
+        elem.classList.remove("CCFrameBZoom");
+      } else {
+        elem.classList.add("CCFrameBZoom");
+      }
+      break;
+    case '4C':
+      var elem = document.getElementById("ContentFrame4C");
+      if (elem.classList.contains("CCFrameCZoom") == true) {
+        elem.classList.remove("CCFrameCZoom");
+      } else {
+        elem.classList.add("CCFrameCZoom");
+      }
+      break;
+    case '4D':
+      var elem = document.getElementById("ContentFrame4D");
+      if (elem.classList.contains("CCFrameDZoom") == true) {
+        elem.classList.remove("CCFrameDZoom");
+      } else {
+        elem.classList.add("CCFrameDZoom");
+      }
+      break;
+    case '4E':
+      var elem = document.getElementById("ContentFrame4E");
+      if (elem.classList.contains("CCFrameEZoom") == true) {
+        elem.classList.remove("CCFrameEZoom");
+      } else {
+        elem.classList.add("CCFrameEZoom");
+      }
+      break;
+    case '4F':
+      var elem = document.getElementById("ContentFrame4F");
+      if (elem.classList.contains("CCFrameFZoom") == true) {
+        elem.classList.remove("CCFrameFZoom");
+      } else {
+        elem.classList.add("CCFrameFZoom");
+      }
+      break;
+    case '4G':
+      var elem = document.getElementById("ContentFrame4G");
+      if (elem.classList.contains("CCFrameGZoom") == true) {
+        elem.classList.remove("CCFrameGZoom");
+      } else {
+        elem.classList.add("CCFrameGZoom");
+      }
+      break;
+    case '4H':
+      var elem = document.getElementById("ContentFrame4H");
+      if (elem.classList.contains("CCFrameHZoom") == true) {
+        elem.classList.remove("CCFrameHZoom");
+      } else {
+        elem.classList.add("CCFrameHZoom");
+      }
+      break;
+    case '4I':
+      var elem = document.getElementById("ContentFrame4I");
+      if (elem.classList.contains("CCFrameIZoom") == true) {
+        elem.classList.remove("CCFrameIZoom");
+      } else {
+        elem.classList.add("CCFrameIZoom");
+      }
+      break;
+    case '4J':
+      var elem = document.getElementById("ContentFrame4J");
+      if (elem.classList.contains("CCFrameJZoom") == true) {
+        elem.classList.remove("CCFrameJZoom");
+      } else {
+        elem.classList.add("CCFrameJZoom");
+      }
+      break;
+    case '4K':
+      var elem = document.getElementById("ContentFrame4K");
+      if (elem.classList.contains("CCFrameKZoom") == true) {
+        elem.classList.remove("CCFrameKZoom");
+      } else {
+        elem.classList.add("CCFrameKZoom");
+      }
+      break;
+    case '4L':
+      var elem = document.getElementById("ContentFrame4L");
+      if (elem.classList.contains("CCFrameLZoom") == true) {
+        elem.classList.remove("CCFrameLZoom");
+      } else {
+        elem.classList.add("CCFrameLZoom");
+      }
+      break;
+  }
+}
+(function() {
+
+  var $$ = function(selector, context) {
+    var context = context || document;
+    var elements = context.querySelectorAll(selector);
+    return [].slice.call(elements);
+  };
+
+  function _fncSliderInit($slider, options) {
+    var prefix = ".fnc-";
+
+    var $slider = $slider;
+    var $slidesCont = $slider.querySelector(prefix + "slider__slides");
+    var $slides = $$(prefix + "slide", $slider);
+    var $controls = $$(prefix + "nav__control", $slider);
+    var $controlsBgs = $$(prefix + "nav__bg", $slider);
+    var $progressAS = $$(prefix + "nav__control-progress", $slider);
+
+    var numOfSlides = $slides.length;
+    var curSlide = 1;
+    var sliding = false;
+    var slidingAT = +parseFloat(getComputedStyle($slidesCont)["transition-duration"]) * 1000;
+    var slidingDelay = +parseFloat(getComputedStyle($slidesCont)["transition-delay"]) * 1000;
+
+    var autoSlidingActive = false;
+    var autoSlidingTO;
+    var autoSlidingDelay = 5000; // default autosliding delay value
+    var autoSlidingBlocked = false;
+
+    var $activeSlide;
+    var $activeControlsBg;
+    var $prevControl;
+
+    function setIDs() {
+      $slides.forEach(function($slide, index) {
+        $slide.classList.add("fnc-slide-" + (index + 1));
+      });
+
+      $controls.forEach(function($control, index) {
+        $control.setAttribute("data-slide", index + 1);
+        $control.classList.add("fnc-nav__control-" + (index + 1));
+      });
+
+      $controlsBgs.forEach(function($bg, index) {
+        $bg.classList.add("fnc-nav__bg-" + (index + 1));
+      });
+    };
+
+    setIDs();
+
+    function afterSlidingHandler() {
+      $slider.querySelector(".m--previous-slide").classList.remove("m--active-slide", "m--previous-slide");
+      $slider.querySelector(".m--previous-nav-bg").classList.remove("m--active-nav-bg", "m--previous-nav-bg");
+
+      $activeSlide.classList.remove("m--before-sliding");
+      $activeControlsBg.classList.remove("m--nav-bg-before");
+      $prevControl.classList.remove("m--prev-control");
+      $prevControl.classList.add("m--reset-progress");
+      var triggerLayout = $prevControl.offsetTop;
+      $prevControl.classList.remove("m--reset-progress");
+
+      sliding = false;
+      var layoutTrigger = $slider.offsetTop;
+
+      if (autoSlidingActive && !autoSlidingBlocked) {
+        setAutoslidingTO();
+      }
+    };
+
+    function performSliding(slideID) {
+      if (sliding) return;
+      sliding = true;
+      window.clearTimeout(autoSlidingTO);
+      curSlide = slideID;
+
+      $prevControl = $slider.querySelector(".m--active-control");
+      $prevControl.classList.remove("m--active-control");
+      $prevControl.classList.add("m--prev-control");
+      $slider.querySelector(prefix + "nav__control-" + slideID).classList.add("m--active-control");
+
+      $activeSlide = $slider.querySelector(prefix + "slide-" + slideID);
+      $activeControlsBg = $slider.querySelector(prefix + "nav__bg-" + slideID);
+
+      $slider.querySelector(".m--active-slide").classList.add("m--previous-slide");
+      $slider.querySelector(".m--active-nav-bg").classList.add("m--previous-nav-bg");
+
+      $activeSlide.classList.add("m--before-sliding");
+      $activeControlsBg.classList.add("m--nav-bg-before");
+
+      var layoutTrigger = $activeSlide.offsetTop;
+
+      $activeSlide.classList.add("m--active-slide");
+      $activeControlsBg.classList.add("m--active-nav-bg");
+
+      setTimeout(afterSlidingHandler, slidingAT + slidingDelay);
+    };
+
+
+
+    function controlClickHandler() {
+      if (sliding) return;
+      if (this.classList.contains("m--active-control")) return;
+      if (options.blockASafterClick) {
+        autoSlidingBlocked = true;
+        $slider.classList.add("m--autosliding-blocked");
+      }
+
+      var slideID = +this.getAttribute("data-slide");
+
+      performSliding(slideID);
+    };
+
+    $controls.forEach(function($control) {
+      $control.addEventListener("click", controlClickHandler);
+    });
+
+    function setAutoslidingTO() {
+      window.clearTimeout(autoSlidingTO);
+      var delay = +options.autoSlidingDelay || autoSlidingDelay;
+      curSlide++;
+      if (curSlide > numOfSlides) curSlide = 1;
+
+      autoSlidingTO = setTimeout(function() {
+        performSliding(curSlide);
+      }, delay);
+    };
+
+    if (options.autoSliding || +options.autoSlidingDelay > 0) {
+      if (options.autoSliding === false) return;
+
+      autoSlidingActive = true;
+      setAutoslidingTO();
+
+      $slider.classList.add("m--with-autosliding");
+      var triggerLayout = $slider.offsetTop;
+
+      var delay = +options.autoSlidingDelay || autoSlidingDelay;
+      delay += slidingDelay + slidingAT;
+
+      $progressAS.forEach(function($progress) {
+        $progress.style.transition = "transform " + (delay / 1000) + "s";
+      });
+    }
+
+    $slider.querySelector(".fnc-nav__control:first-child").classList.add("m--active-control");
+
+  };
+
+  var fncSlider = function(sliderSelector, options) {
+    var $sliders = $$(sliderSelector);
+
+    $sliders.forEach(function($slider) {
+      _fncSliderInit($slider, options);
+    });
+  };
+
+  window.fncSlider = fncSlider;
+}());
+
+fncSlider(".example-slider", {autoSlidingDelay: 4000});
+
+var $demoCont = document.querySelector(".demo-cont");
+
+[].slice.call(document.querySelectorAll(".fnc-slide__action-btn")).forEach(function($btn) {
+  $btn.addEventListener("click", function() {
+    $demoCont.classList.toggle("credits-active");
+  });
+});
